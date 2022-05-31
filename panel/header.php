27245804589a24dbbb7dff708cc3c3fd25856324
YahyaCanN <89393704+YahyaCanN@users.noreply.github.com>
2022-05-31 12:03:35 +0300

    Add files via upload

---
+++ panel/header.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<nav>
+    <div class="container">
+        <div class="back"> <a href="../html/anasayfa.html" style="color:#000000;">Siteye Geri Dönün</a></div>
+       
+        <div id="overlay"></div>
+       
+        
+    </div>
+</nav>
